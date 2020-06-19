138eb9837021db0cb883e4bd075ce1772a92d0e1
ranguli <hello@joshmurphy.ca>
2020-06-19 12:46:15 -0230

    Linted Windrose.kt

--- src/main/kotlin/ca/ranguli/windrose/Windrose.kt
+++ src/main/kotlin/ca/ranguli/windrose/Windrose.kt
@@ -1,21 +1,19 @@
 package ca.ranguli.windrose
 
-import kotlin.math.roundToInt
-
-import org.bukkit.plugin.java.JavaPlugin
-import org.bukkit.entity.Player
 import org.bukkit.ChatColor
-import org.bukkit.command.CommandSender
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
-
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+import org.bukkit.plugin.java.JavaPlugin
+import kotlin.math.roundToInt
 
 class Windrose : JavaPlugin(), CommandExecutor {
 
     override fun onEnable() {
         logger.info(description.name + " has been enabled.")
         getCommand("loc")?.setExecutor(this)
-    }	
+    }
 
     override fun onCommand(sender: CommandSender, command: Command, commandLabel: String, args: Array<String>): Boolean {
         if (sender is Player) {
@@ -31,6 +29,5 @@ class Windrose : JavaPlugin(), CommandExecutor {
 
     override fun onDisable() {
         logger.info(description.name + " has been disabled.")
-    }	
-	
+    }
 }
